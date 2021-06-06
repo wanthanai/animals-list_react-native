ecedb566e721ee70b9214f7767859c9e81df3414
wanthanai <wanthanai4work@gmail.com>
2021-06-06 16:39:24 +0700

    create state animals and render items on listItems component

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 // component
 import Header from './components/Header'
+import ListItems from './components/ListItems'
+// uuid
+import uuid from 'react-native-uuid'
+
 
 //! React functional component
 const App = () => {
+  // state
+  const [items, setItems] = useState([
+    {id: uuid.v4(), text: 'Pig'},
+    {id: uuid.v4(), text: 'Cat'},
+    {id: uuid.v4(), text: 'Dog'},
+    {id: uuid.v4(), text: 'Bird'},
+  ]);
+
   return (
-    <View>
+    <View style={styles.container}>
       <Header/>
+      <FlatList data={items} renderItem={({item}) => <ListItems item={item} />} />
     </View>
   )
 }
+
+// styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
 
 export default App;
---
+++ components/ListItems.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+// icon 
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+export default function ListItems({ item }) {
+    return (
+        <TouchableOpacity style={styles.container
+        }>
+            <View>
+                <Text style={styles.text}>{item.text}</Text>
+                <Text style={styles.icon}>{item.id}</Text>
+            </View>
+            <Icon name="remove" size={20} color="firebrick" />
+        </TouchableOpacity>
+    )
+}
+
+
+// styles 
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 15,
+        backgroundColor: '#f8f8f8',
+        borderBottomWidth: 1,
+        borderColor: '#eeee'
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: '600'
+    },
+    icon: {
+        color: 'rgba(0, 0, 0, 0.4)'
+    }
+})
